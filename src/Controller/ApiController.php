5016f174a0e1d42a0f725d5a1516905638d64b16
forkrz <forkrz8@gmail.com>
2022-03-13 21:33:55 +0100

    Barcodes class added

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -1,21 +1,19 @@
 <?php
 namespace App\Controller;
 
+use App\Utils\Barcodes;
 use App\Utils\DataCheck;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Com\Tecnick\Barcode\Barcode;
-use Exception;
-
 #[Route("/api")]
 
 class ApiController extends AbstractController
 {
 
     #[Route('/ean8', name: 'EAN8', methods:"GET|POST")]
-    public function index(Request $request,DataCheck $dataCheck):Response
+    public function index(Request $request,DataCheck $dataCheck,Barcodes $barcodes):Response
     {
         $response = new Response;
 
@@ -24,22 +22,10 @@ class ApiController extends AbstractController
 
         if(!$dataCheck->checkIfInputIsNotEmpty($value,$typeOfBarcode)){
             return  $response->setContent('Both values cannot be empty');
+        }else{
+            return $barcodes->createBarcode($value,$typeOfBarcode);
         }
-        $barcode = new Barcode();
-        try{
-            $bobj = $barcode->getBarcodeObj(
-                $typeOfBarcode,
-                $value,
-                1200,
-                400,
-                'black',                       
-                )->setBackgroundColor('white');
-                $file_webp = __DIR__ . '../Utils/Pictures/barcode.webp';
-                file_put_contents($file_webp, $bobj->getPngData());
-        }catch(Exception $e){
-            return  $response->setContent($e->getMessage());
-        }
-            return  $response->setContent('Barcode generated');
-
+        
+        
     }
 }
